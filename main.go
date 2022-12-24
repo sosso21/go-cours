f3c54f7d8ce1132b608e55f93ea5fba3ae61f4db
sosso21 <sofianetop21.st@gmail.com>
2022-12-24 23:55:04 +0100

    :sparkles: (feat): anonyme function map

--- main.go
+++ main.go
@@ -2,11 +2,34 @@ package main
 
 import (
   "fmt"
-  
 )
 
+
+
 func main() {
-  name := "Hermes"
-  fmt.Println(name)
-     SayHello(name)  
+  
+hello := func () (string) {
+  return  "hello"
+}
+fmt.Printf("%v \n", hello() )
+
+  months := map[string]int{
+    "jan": 31 ,
+    "feb": 28,
+    "mar": 31,
+    "apr":30,
+    "may": 31,
+    "jun":30,
+    "jul": 31,
+    "aug":31,
+    "sep":30,
+    "oct": 31,
+    "nov":30,
+    "dev": 31,
+  }
+
+  for  month, days := range months {
+    fmt.Printf("the month %s have %v days \n",  month , days )
+  }
+
 }
