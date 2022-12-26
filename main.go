51f961f71de7679b93236f0a98a8e8c25644e8c2
sosso21 <sofianetop21.st@gmail.com>
2022-12-26 02:31:56 +0100

    :sparkles: generic

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
   "errors"
   
 )
+ //        [T float64 | int64] 
+ // ?   ~ : can accept float32 
 
-func Divide [T float32 | int32](a , b T) (T , error) {
+func Divide [T ~float64 | ~int64](a , b T) (T , error) {
   if b == 0 {
     return 0.0 , errors.New("Invalid Name")
   }
@@ -14,7 +16,8 @@ func Divide [T float32 | int32](a , b T) (T , error) {
 }
 
 func main() {
-   result , err := Divide[float32](22.00, 7.00)
+  // result , err := Divide(22.00, 7.00)
+  result , err := Divide[float64](22.00, 7.00)
    if  err != nil {
     panic(err)
    }
